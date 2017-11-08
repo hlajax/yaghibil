52d4a909591aef83eb974268760aa4aaa72ab57f
Иван Бишевац <ivan.bisevac@gmail.com>
2017-11-08 11:22:26 +0100

    Display total for current month

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ApplicationRecord
+  def self.total_for_current_month
+    where("created_at > ? AND created_at < ?", Time.current.beginning_of_month, Time.current.end_of_month).sum(:price)
+  end
 end
