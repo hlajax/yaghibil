3c39770adb6cb812164b6bb09af79e0de6585b5a
Иван Бишевац <ivan.bisevac@gmail.com>
2017-11-15 23:50:57 +0100

    Change order of expenses to billed_at instead of created_at

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-  default_scope { order(created_at: :asc) }
+  default_scope { order(billed_at: :asc) }
 
   def self.total_for_current_month
     where("created_at > ? AND created_at < ?", Time.current.beginning_of_month, Time.current.end_of_month).sum(:price)
