3ea4565ab7d0641366fdf7bde938fecffe57b7dc
Иван Бишевац <ivan.bisevac@gmail.com>
2017-12-28 20:51:44 +0100

    Sum by billed at not created at

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
-  default_scope { order(billed_at: :asc) }
+  default_scope { order(billed_at: :desc) }
 
   def self.total_for_current_month
-    where("created_at > ? AND created_at < ?", Time.current.beginning_of_month, Time.current.end_of_month).sum(:price)
+    where("billed_at > ? AND billed_at < ?", Time.current.beginning_of_month, Time.current.end_of_month).sum(:price)
   end
 end
